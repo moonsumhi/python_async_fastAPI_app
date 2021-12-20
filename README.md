c6a0f7255df3e0d5c89a50bf0503626fbef7fa8c
sumee <moonsumhi@gmail.com>
2021-12-20 09:44:09 +0900

    [CREATE] app

---
+++ README.md
@@ -0,0 +1,2 @@
+# python_async_fastAPI_app
+learning python async and fastAPI with inflearn 
---
+++ app/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from app.models import mongodb
+from app.models.book import BookModel
+from app.book_scrap import NaverBookScraper
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app = FastAPI()
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "콜렉터 북북이"}
+    )
+
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    keyword = q
+    if not keyword:
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "title": "콜렉터스 북북이"}
+        )
+    if await mongodb.engine.find_one(BookModel, BookModel.keyword == keyword):
+        books = await mongodb.engine.find(BookModel, BookModel.keyword == keyword)
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "title": "콜렉터 북북이", "books": books}
+        )
+
+    naver_book_scraper = NaverBookScraper()
+    books = await naver_book_scraper.search(keyword, 10)
+    book_models = []
+    for book in books:
+        book_model = BookModel(
+            keyword=keyword,
+            publisher=book["publisher"],
+            price=book["price"],
+            image=book["image"],
+        )
+        book_models.append(book_model)
+    await mongodb.engine.save_all(book_models)
+
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "콜렉터 북북이", "books": books}
+    )
+
+
+# app이 처음 실행될 때 실행됨
+@app.on_event("startup")
+def on_app_start():
+    mongodb.connect()
+
+
+# server가 shutdown 됐을 때 사용되는 이벤트
+@app.on_event("shutdown")
+async def on_app_shutdown():
+    mongodb.close()
